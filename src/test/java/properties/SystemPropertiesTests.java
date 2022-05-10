e958088598c300c023711feab993adb6994fc3fd
igorsid <sidgarri@gmail.com>
2022-05-10 23:37:46 +0800

    Add Credentials, property, ownerTests

---
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -0,0 +1,15 @@
+package properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+
+    @Test
+    @Tag("RegistrationFormTests")
+    void TestBase() {
+        String baseUrl = System.getProperty("url", "https://demoqa.com");
+        String version = System.getProperty("version", "100");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+    }
+}
